0edc72c64a21d0680e75a2a42b82c381f5af4e7f
Umar Mr. Wasim <umar.wasim@corp.wizlinx.com>
2023-12-22 19:54:20 +0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using CodeFirstApproach.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CodeFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StudentDBContext studentDB;
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController(StudentDBContext studentDB)
+        {
+            this.studentDB = studentDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var std = await studentDB.Students.ToListAsync();
+            return View(std);
+        }
+
+        //Details Method
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+            var std = await studentDB.Students.FirstOrDefaultAsync(x => x.Id == id);
+            if(std == null)
+            {
+                return NotFound();
+            }
+            return View(std);
+        }
+
+        //Create Methods
+        public IActionResult Create()
+        {
+            
+            return View();
+        } 
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student std)
+        {
+            if(ModelState.IsValid)
+            {
+               await studentDB.Students.AddAsync(std);
+                await studentDB.SaveChangesAsync();
+                TempData["insert_success"] = "Inserted new recoed";
+                return RedirectToAction("Index","Home");
+            }
+            return View(std);
+        }
+
+        //Edit Methods
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+            var std = await studentDB.Students.FindAsync(id);
+            if (std == null)
+            {
+                return NotFound();
+            }
+            return View(std);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, Student std)
+        {
+            if(id != std.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                studentDB.Update(std);
+                await studentDB.SaveChangesAsync();
+                TempData["edit_success"] = "Edited record";
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        //Delete Methods
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+            var std = await studentDB.Students.FirstOrDefaultAsync(x => x.Id == id);
+            if (std == null)
+            {
+                return NotFound();
+            }
+            return View(std);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmation(int? id)
+        {
+
+            var std = await studentDB.Students.FindAsync(id);
+            if (std != null)
+            {
+                studentDB.Students.Remove(std);
+            }
+            await studentDB.SaveChangesAsync();
+            TempData["delete_success"] = "Deleted record";
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StudentDBContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace CodeFirstApproach.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
+
